dc20a89661dcf40c05c7ea41d8b36e652dc6b280
Martin <mkmartinoes@gmail.com>
2022-05-24 12:16:32 +0300

    st: routes complete

--- src/components/BrowserRoutes/RouterSetup.js
+++ src/components/BrowserRoutes/RouterSetup.js
@@ -3,6 +3,9 @@ import React, {Fragment} from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "../home/Home";
+import Login from "../login/Login";
+import SignUp from "../signup/SignUp";
+import Test from "../test/Test"
 
 
 function RouterSetUp(){
@@ -10,13 +13,16 @@ function RouterSetUp(){
     <Router>
       <Fragment>
         <Routes>
-          <Route path="/" element = { <Home />}>
+          <Route path="/" element={<Home />}>
             {/* <Home /> */}
           </Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/test" element={<Test />}></Route>
         </Routes>
       </Fragment>
     </Router>
-  )
+  );
 }
 
 export default RouterSetUp
