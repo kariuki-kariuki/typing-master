55e4613b18b68fd70f561a4c491d53c84086342d
Martin <mkmartinoes@gmail.com>
2022-05-25 15:15:05 +0300

    st: home page design

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,32 +3,10 @@ import Turbo from '../turbo/Turbo';
 function Home(){
 
   return (
-    <div className="row">
-      <div className="col-sm-6">
-        <div className="container-main half-content d-flex flex-row align-items-center">
-          <div className="card-custom">
-            <h1>My Card Content</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-              fugiat iusto fuga praesentium optio, eaque rerum! Provident
-              similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-              eius earum ut molestias architecto voluptate aliquam nihil,
-              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-              tenetur error, harum nesciunt ipsum debitis quas aliquid.
-              Reprehenderit, quia.{" "}
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="col-sm-6">
-        <div className="container-main half-content d-flex flex-row align-items-center">
-          <div className="card-custom">
-            <h1>My Card Content</h1>
-            <Turbo />
-          </div>
-        </div>
+    <div className="container-main home">
+      <div className='card-custom'>
+        <h1>Worrier KeyBoard</h1>
+        <p>Keyboard mastery on steroids</p>
       </div>
     </div>
   );
