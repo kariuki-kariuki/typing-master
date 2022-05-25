4764e10ba132862b341e6c76529826576ad5dfc8
Martin <mkmartinoes@gmail.com>
2022-05-25 12:09:18 +0300

    st: graph scale

--- src/components/graph/Graph.js
+++ src/components/graph/Graph.js
@@ -5,27 +5,35 @@ function Graph(){
   return (
     <div className="graph">
       <span className="spanone d-flex flex-row justify-content-around">
-        <span>90 %</span>
-        <span>80 %</span>
-        <span>70 %</span>
-        <span>60 %</span>
-        <span>50 %</span>
-        <span>40 %</span>
-        <span>30 %</span>
-        <span>20 %</span>
         <span>10 %</span>
+        <span>20 %</span>
+        <span>30 %</span>
+        <span>40 %</span>
+        <span>50 %</span>
+        <span>60 %</span>
+        <span>70 %</span>
+        <span>80 %</span>
+        <span>90 %</span>
       </span>
       <br />
-      <span className="spantwo"><span className="span2">Hello</span></span>
+      <span className="spantwo">
+        <span className="span2">Hello</span>
+      </span>
       <br />
 
-      <span className="spanthree">Hello</span>
+      <span className="spanthree">
+        <span className="span3">Hello</span>
+      </span>
       <br />
 
-      <span className="spanfour">Hello</span>
+      <span className="spanfour">
+        <span className="span4">Hello</span>
+      </span>
       <br />
 
-      <span className="spanfive">Hello</span>
+      <span className="spanfive">
+        <span className="span5">Hello</span>
+      </span>
       <br />
     </div>
   );
