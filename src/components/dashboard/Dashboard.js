4f65b63bce8665912e3bb3799034d5a8acc5f949
Martin <mkmartinoes@gmail.com>
2022-05-25 15:40:33 +0300

    st: dashboard css styling

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,11 +3,11 @@ import Graph from "../graph/Graph";
 function Dashboard(){
 
   return (
-    <div className="container-main bg-dark h-100">
-      <div className="header bg-dark text-white text-center">
+    <div className="container-main  h-100">
+      <div className="header text-white text-center">
         <h1>Statiscica</h1>
       </div>
-      <div className="row bg-dark">
+      <div className="row ">
         <div className="col-sm-4">
           <div className="mycard d-flex align-items-center flex-row justify-content-center">
             <div className="circle text-center d-flex align-items-center flex-row justify-content-center">
@@ -17,7 +17,9 @@ function Dashboard(){
           </div>
         </div>
         <div className="=col-sm-8">
+          <div className="myGraph">
             <Graph />
+          </div>
         </div>
       </div>
     </div>
