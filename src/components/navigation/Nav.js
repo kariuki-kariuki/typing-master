1d96348b9cb5b699812b753c9bfa47c2ecf653a9
Martin <mkmartinoes@gmail.com>
2022-05-25 14:57:45 +0300

    st: asset image

--- src/components/navigation/Nav.js
+++ src/components/navigation/Nav.js
@@ -1,6 +1,7 @@
 
 import Button from "../custombtn/Button"
 import { NavLink } from "react-router-dom";
+import Test from "../test/Test";
 import "./Nav.css"
 
 function Nav(){
@@ -8,15 +9,25 @@ function Nav(){
   return (
     <nav className="container-main p-2 bg-dark ">
       <br />
-        <NavLink to="/">
-          <Button text="Home" backColor="#0000" bradius="5px" />
-        </NavLink>
-        <NavLink to="/login">
-          <Button text="Login" backColor="#0000" bradius="5px" />
-        </NavLink>
-        <NavLink to= "/dashboard">
-          <Button text = "Dashboard" bradius= "5px"/>
-        </NavLink>
+      <NavLink to="/">
+        <Button text="Home"  bradius="5px" />
+      </NavLink>
+      <br />
+      <br />
+      <NavLink to= "/test">
+        <Button text='home'  bradius = "5px" />
+      </NavLink>
+      <br />
+      <br />
+      <NavLink to="/login">
+        <Button text="Login"  bradius="5px" />
+      </NavLink>
+      <br />
+      <br />
+
+      <NavLink to="/dashboard">
+        <Button text="Dashboard" bradius="5px" />
+      </NavLink>
 
       <br />
       <hr />
