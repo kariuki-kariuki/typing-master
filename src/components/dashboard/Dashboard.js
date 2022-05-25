150f3419c7cbf2386c4fe26b2a5a60dbaf68568e
Martin <mkmartinoes@gmail.com>
2022-05-25 11:25:36 +0300

    st: graph design

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,28 +1,7 @@
 import "./dashboard.css"
-import { Chart, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+import Graph from "../graph/Graph";
 function Dashboard(){
-  var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-  var yValues = [55, 49, 44, 24, 15];
-  var barColors = ["red", "green", "blue", "orange", "brown"];
-  new Chart("myChart", {
-    type: "bar",
-    data: {
-      labels: xValues,
-      datasets: [
-        {
-          backgroundColor: barColors,
-          data: yValues,
-        },
-      ],
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-        text: "World Wine Production 2018",
-      },
-    },
-  });
+
   return (
     <div className="container-main bg-dark">
       <div className="row">
@@ -33,10 +12,10 @@ function Dashboard(){
               <span className="tText">tests</span>
             </div>
           </div>
-          <div className="=col-sm-9">
-            <div className="conatainer-main">
-              <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
-            </div>
+        </div>
+        <div className="=col-sm-9">
+          <div className="conatainer-main">
+            <Graph />
           </div>
         </div>
       </div>
---
+++ src/components/graph/Graph.js
@@ -0,0 +1,34 @@
+import "./graph.css"
+
+function Graph(){
+
+  return (
+    <div className="graph">
+      <span className="spanone d-flex flex-row justify-content-around">
+        <span>90 %</span>
+        <span>80 %</span>
+        <span>70 %</span>
+        <span>60 %</span>
+        <span>50 %</span>
+        <span>40 %</span>
+        <span>30 %</span>
+        <span>20 %</span>
+        <span>10 %</span>
+      </span>
+      <br />
+      <span className="spantwo">Hello</span>
+      <br />
+
+      <span className="spanthree">Hello</span>
+      <br />
+
+      <span className="spanfour">Hello</span>
+      <br />
+
+      <span className="spanfive">Hello</span>
+      <br />
+    </div>
+  );
+}
+
+export default Graph
