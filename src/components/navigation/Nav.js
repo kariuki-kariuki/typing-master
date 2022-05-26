2c5c51033c8619b56db8a18c39a1279e9f777fee
Martin <mkmartinoes@gmail.com>
2022-05-27 01:10:36 +0300

    Nav styling

--- src/components/navigation/Nav.js
+++ src/components/navigation/Nav.js
@@ -4,33 +4,36 @@ import { NavLink } from "react-router-dom";
 import "./Nav.css"
 
 function Nav(){
+  // const styles = {
+  //   ba
+  // }
 
   return (
     <nav className="container-main p-5 ">
       <br />
-      <NavLink to="/">
-        Home
+      <NavLink style={{ color: "white", fontSize: "32px" }} to="/">
+        #Home
       </NavLink>
       <br />
       <br />
-      <NavLink to="/test">
-        Test
+      <NavLink style={{ color: "white", fontSize: "32px" }} to="/test">
+        #Test
       </NavLink>
       <br />
       <br />
-      <NavLink to="/login">
-        Login
+      <NavLink style={{ color: "white", fontSize: "32px" }} to="/login">
+        #Login
       </NavLink>
       <br />
       <br />
 
-      <NavLink to="/dashboard">
-        Dashboard
+      <NavLink style={{ color: "white", fontSize: "32px" }} to="/dashboard">
+        #Dashboard
       </NavLink>
       <br />
       <br />
-      <NavLink to="/signup">
-        SignUp
+      <NavLink style={{ color: "white", fontSize: "32px" }} to="/signup">
+        #SignUp
       </NavLink>
 
       <br />
